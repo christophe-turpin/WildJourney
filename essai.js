97238f1af63a8fdbda2f675665793073c50c42bf
Chris356 <daisukeleblonx@gmail.com>
2020-03-20 15:38:45 +0100

    test

--- essai.js
+++ essai.js
@@ -37,24 +37,19 @@ for (i = 0; i < dropdown.length; i++) {
 //     window.location = $("a").attr("href")
 // };
 
-function switchTheme(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'ac');
-    }
-   
-}
-
 function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementsByClassName("searchbar");
     filter = input.value.toUpperCase();
     ul = document.getElementById("ccList");
     li = ul.getElementsByTagName("li");
-        if (txtValue.toUpperCase().indexOf(filter) > 0) {
-            document.documentElement.setAttribute('data-theme', 'search');
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
-            document.documentElement.setAttribute('data-theme', 'nosearch');
             li[i].style.display = "none";
         }
     }
+}
