fccba2fea7fa314504220a0c8c6ec4cb3f78bb20
Chris356 <daisukeleblonx@gmail.com>
2020-03-28 16:25:19 +0100

    correction de la fonction carousel

--- essai.js
+++ essai.js
@@ -58,12 +58,21 @@ function mobile_link() {
 }
 
 window.onload =
+    // function () {
+    //     $('#carouselExampleCaptions').Carousel(
+    //         {
+    //             items: 'img',
+    //             reflections: .5,
+    //             rotationSpeed: 1.8
+    //         }
+    //     );
+
     function () {
-        $('#carouselExampleCaptions').Carousel(
-            {
-                items: 'img',
-                reflections: .5,
-                rotationSpeed: 1.8
-            }
-        );
-    }
+        // Affichage du sous menu en douceur
+        jQuery('ul.nav li.dropdown').hover(function () {
+            jQuery(this).find('.jqueryFadeIn').stop(true, true).delay(200).fadeIn();
+        }, function () {
+            jQuery(this).find('.jqueryFadeIn').stop(true, true).delay(200).fadeOut();
+        });
+
+    };
